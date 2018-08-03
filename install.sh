7b75fad1c3774016efe4a9bf5fe9e2bc4dc02d7c
Jonathan Snook <jonathan@snook.ca>
2018-08-03 15:04:41 -0400

    updated install and bash profile

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ruby sass/sass/sass bash-git-prompt
+
+## Sass path stuff for bash_profile (which should already be in there
+# echo 'export PATH="/usr/local/opt/dart@2/bin:$PATH"' >> ~/.bash_profile
+
+## bash-git-prompt stuff for bash_profile (which should already be in there
+# if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+#   __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
+#   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+# fi
